dc2781931ce1fb732bdd7983def5c3092404b3fe
Munseer-am <munnamunseer7@gmail.com>
2022-11-23 22:01:59 +0530

    Image added

--- motivation.py
+++ motivation.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 import random
-import requests
+# import requests
 import sys
 from quotes import quotes
 from PyQt5.QtWidgets import *
@@ -12,12 +12,17 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle("Motivational Quotes")
-        pixmap = QPixmap()
-        pixmap.loadFromData(requests.get("https://munseer.pythonanywhere.com/static/logo.jpg").content)
-        icon = QIcon(pixmap)
-        self.setWindowIcon(icon)
+        label = QLabel(self)
+        pixmap = QPixmap('cat.jpg')
+        label.setPixmap(pixmap)
+        self.setCentralWidget(label)
+        self.resize(pixmap.width(), pixmap.height())
 
+# try:
+    # request = requests.get("https://google.com/", timeout=5)
 app = QApplication(sys.argv)
 w = MainWindow()
 w.show()
 app.exec()
+# except (requests.ConnectionError, requests.Timeout) as e:
+    # print("check your internet connection")
