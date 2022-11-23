130f1ca203c35304227644f47905eb37d0c8d39d
Munseer-am <munnamunseer7@gmail.com>
2022-11-23 22:55:53 +0530

    Image Fetching Complete

--- motivation.py
+++ motivation.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 import random
-# import requests
+import requests
 import sys
 from quotes import quotes
 from PyQt5.QtWidgets import *
@@ -12,11 +12,25 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle("Motivational Quotes")
-        label = QLabel(self)
-        pixmap = QPixmap('cat.jpg')
-        label.setPixmap(pixmap)
-        self.setCentralWidget(label)
-        self.resize(pixmap.width(), pixmap.height())
+        self.label = QLabel(self)
+        self.r = requests.get("https://munseer.pythonanywhere.com/api/")
+        pixmap1 = QPixmap()
+        pixmap1.loadFromData(self.r.content)
+        pixmap = pixmap1.scaled(self.width(), self.height())
+        self.label.setPixmap(pixmap)
+        self.setCentralWidget(self.label)
+        self.label.setMinimumSize(1, 1)
+        # self.setFixedHeight(480)
+        # self.setFixedWidth(620)
+        self.quitSc = QShortcut(QKeySequence('Ctrl+Q'), self)
+        self.quitSc.activated.connect(QApplication.instance().quit)
+
+    def resizeEvent(self, event):
+        pixmap1 = QPixmap()
+        pixmap1.loadFromData(self.r.content)
+        self.pixmap = pixmap1.scaled(self.width(), self.height())
+        self.label.setPixmap(self.pixmap)
+        self.label.resize(self.width(), self.height())
 
 # try:
     # request = requests.get("https://google.com/", timeout=5)
