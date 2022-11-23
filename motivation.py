41429d00b3ec94977de7f008140d3f027f916732
Munseer-am <munnamunseer7@gmail.com>
2022-11-23 23:01:13 +0530

    completed

--- motivation.py
+++ motivation.py
@@ -11,7 +11,11 @@ from PyQt5.QtCore import *
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.setWindowTitle("Motivational Quotes")
+        self.setWindowTitle("Programming memes")
+        pixmap2 = QPixmap()
+        pixmap2.loadFromData(requests.get("https://munseer.pythonanywhere.com/static/logo.jpg").content)
+        icon = QIcon(pixmap2)
+        self.setWindowIcon(icon)
         self.label = QLabel(self)
         self.r = requests.get("https://munseer.pythonanywhere.com/api/")
         pixmap1 = QPixmap()
@@ -32,11 +36,11 @@ class MainWindow(QMainWindow):
         self.label.setPixmap(self.pixmap)
         self.label.resize(self.width(), self.height())
 
-# try:
-    # request = requests.get("https://google.com/", timeout=5)
-app = QApplication(sys.argv)
-w = MainWindow()
-w.show()
-app.exec()
-# except (requests.ConnectionError, requests.Timeout) as e:
-    # print("check your internet connection")
+try:
+    request = requests.get("https://google.com/", timeout=5)
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    app.exec()
+except (requests.ConnectionError, requests.Timeout) as e:
+    print("check your internet connection")
